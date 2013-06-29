6e1f31043e63718427ccaacb431328115549d0c8
skeevy420 <r.allen.payne@gmail.com>
2013-06-28 19:36:41 -0500

    fix derp

--- msm8960-common-vendor-blobs.mk
+++ msm8960-common-vendor-blobs.mk
@@ -142,7 +142,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/msm8960-common/proprietary/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so
 
 PRODUCT_COPY_FILES += \
-    vendor/motorola/msm8960-common/proprietary/app/libmmjpeg.so:system/app/CircleConfig.apk \
+    vendor/motorola/msm8960-common/proprietary/app/CircleConfig.apk:system/app/CircleConfig.apk \
     vendor/motorola/msm8960-common/proprietary/app/CircleWidget3D.apk:system/app/CircleWidget3D.apk \
     vendor/motorola/msm8960-common/proprietary/app/DataMeterAtt.apk:system/app/DataMeterAtt.apk \
     vendor/motorola/msm8960-common/proprietary/app/HomeConfig.apk:system/app/HomeConfig.apk \
